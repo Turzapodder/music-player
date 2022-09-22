06b1951b8de7f57eb2b770860d5bf44a665f3a7c
Adiba001 <adiba001.bd@gmail.com>
2022-09-23 02:17:09 +0600

    Basic Pages, App Drawer & Bottom Navigation bar Created

---
+++ lib/edit-profile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class editProfile extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Edit-profile',
+        style: TextStyle(fontSize: 50),
+      ),
+    );
+  }
+}
---
+++ lib/settings.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class settingsPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Settings Page',
+        style: TextStyle(fontSize: 50),
+      ),
+    );
+  }
+}
